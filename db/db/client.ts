73e59103fae94999dfb8d5107d59dff947561e8f
AmericanoHoney <cocoart033@gmail.com>
2025-08-07 17:51:28 +0700

    delete db folder

--- db/db/client.ts
+++
@@ -1,9 +0,0 @@
-
-import { drizzle } from "drizzle-orm/mysql2";
-import * as schema from "@db/schema.js";
-import mysql from "mysql2/promise";
-import { connectionConfig } from "@db/utils.js";
-
-export const dbConn = await mysql.createConnection(connectionConfig);
-
-export const dbClient = drizzle(dbConn, { schema, mode: "default" });
