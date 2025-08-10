deb00028686cd2d3e401989c0281a45c7bc24929
xtxme <TT.icy013@gmail.com>
2025-08-10 09:45:05 +0700

    frontend test done

--- test/cypress.config.ts
+++ test/cypress.config.ts
@@ -6,8 +6,10 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       config.env = config.env || {};
       // you could extract only specific variables and rename them if necessary
-      config.env.FRONTEND_URL = process.env.FRONTEND_URL;
-      config.env.BACKEND_URL = process.env.BACKEND_URL;
+      config.env.FRONTEND_URL =
+        process.env.FRONTEND_URL || "http://localhost:4000";
+      config.env.BACKEND_URL =
+        process.env.BACKEND_URL || "http://localhost:3000";
       console.log("Extended config.env with process.env");
       return config;
     },
